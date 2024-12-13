3e29715b0c91bb3c12d42eda46b57729c17638f6
gabriel.azevedo <g05g12g82@>
2024-12-13 15:23:50 -0300

    ajsutes

--- src/controller/UsuarioController.py
+++ src/controller/UsuarioController.py
@@ -1,4 +1,9 @@
+import os
 import re
+
+from flask import jsonify
+from werkzeug.exceptions import BadRequest, InternalServerError
+
 
 class UsuarioController:
     @staticmethod
@@ -10,5 +15,41 @@ class UsuarioController:
         return bool(re.match(regex, email))
 
     @classmethod
-    def validar_cadastro_inicial(cls, usuario_data):
-        pass
+    def criar_usuario(cls, usuario_data, db):
+        usuario_collection = db.usuario
+
+        login = usuario_data.get('login')
+        senha = usuario_data.get('senha')
+
+        #valida campos
+        if not login or not senha:
+            raise BadRequest("Os campos 'login' e 'senha' são obrigatórios.")
+
+        # Validar o e-mail usando o UserController
+        if not UsuarioController.validar_email(login):
+            raise BadRequest("O campo 'login' deve ser um email válido.")
+
+        #valida duplicado
+        if usuario_collection.find_one({"login": login}):
+            raise BadRequest("Já existe um usuário com este login.")
+
+        usuario = {
+            "login": login,
+            "senha": senha,
+            "nome": usuario_data.get('nome'),
+            "telefone": usuario_data.get('telefone'),
+            "rua": usuario_data.get('rua'),
+            "numero": usuario_data.get('numero'),
+            "bairro": usuario_data.get('bairro'),
+            "cidade": usuario_data.get('cidade'),
+            "loja": os.getenv("loja_id"),
+            "tipo": 'client',
+            "ativo": 1
+        }
+
+        try:
+            result = usuario_collection.insert_one(usuario)
+            usuario['_id'] = str(result.inserted_id)
+            return jsonify(usuario), 201
+        except Exception as e:
+            raise InternalServerError(f"Ocorreu um erro ao criar o usuário: {str(e)}")
--- src/rotas/UsuarioRota.py
+++ src/rotas/UsuarioRota.py
@@ -1,4 +1,3 @@
-# scr/routes/UsuarioRota.py
 from flask import request, jsonify
 from werkzeug.exceptions import InternalServerError, BadRequest
 
@@ -36,43 +35,9 @@ def UsuarioRotas(app, db):
             schema:
               id: Usuario
         """
-        usuario_collection = db.usuario
-        usuario_data = request.get_json()
 
-        if not UsuarioController.validar_cadastro_inicial(usuario_data):
-        login = usuario_data.get('login')
-        senha = usuario_data.get('senha')
+        return UsuarioController.criar_usuario(request.get_json(), db);
 
-        if not login or not senha:
-            raise BadRequest("Os campos 'login' e 'senha' são obrigatórios.")
-
-        # Validar o e-mail usando o UserController
-        if not UsuarioController.validar_email(login):
-            raise BadRequest("O campo 'login' deve ser um email válido.")
-
-        if usuario_collection.find_one({"login": login}):
-            raise BadRequest("Já existe um usuário com este login.")
-
-        usuario = {
-            "login": login,
-            "senha": senha,
-            "nome": usuario_data.get('nome'),
-            "telefone": usuario_data.get('telefone'),
-            "rua": usuario_data.get('rua'),
-            "numero": usuario_data.get('numero'),
-            "bairro": usuario_data.get('bairro'),
-            "cidade": usuario_data.get('cidade'),
-            "loja": os.getenv("loja_id"),
-            "tipo": 'client',
-            "ativo": 1
-        }
-
-        try:
-            result = usuario_collection.insert_one(usuario)
-            usuario['_id'] = str(result.inserted_id)
-            return jsonify(usuario), 201
-        except Exception as e:
-            raise InternalServerError(f"Ocorreu um erro ao criar o usuário: {str(e)}")
 
     # Rota para atualizar o usuário
     @app.route('/usuario/<user_id>', methods=['PUT'])
@@ -97,6 +62,24 @@ def UsuarioRotas(app, db):
                   type: string
                 nome:
                   type: string
+                cpf:
+                  type: string
+                telefone:
+                  type: string
+                rua:
+                    type: string
+                numero:
+                    type: string
+                bairro:
+                    type: string
+                cidade:
+                    type: string
+                loja:
+                    type: string
+                tipo:
+                    type: string
+                status:
+                    type: string
 
         responses:
           200:
